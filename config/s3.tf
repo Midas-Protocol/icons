c04b86dd7ebe367c1faa280af733fccf1a51399a
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-09-20 15:27:05 +0200

    add network and social

--- config/s3.tf
+++ config/s3.tf
@@ -27,13 +27,13 @@ resource "aws_s3_bucket" "s3_bucket" {
 }
 
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
-    bucket = aws_s3_bucket.s3_bucket.id
-    policy = data.aws_iam_policy_document.s3_bucket_policy.json
+  bucket = aws_s3_bucket.s3_bucket.id
+  policy = data.aws_iam_policy_document.s3_bucket_policy.json
 }
 
 resource "aws_s3_bucket_acl" "s3_bucket" {
   bucket = var.domain_name
-  acl = "private"
+  acl    = "private"
 }
 
 resource "aws_s3_bucket_versioning" "s3_bucket" {
@@ -45,27 +45,46 @@ resource "aws_s3_bucket_versioning" "s3_bucket" {
 
 resource "aws_s3_object" "dist" {
   for_each = fileset("${path.module}/../token/", "*")
-  bucket = aws_s3_bucket.s3_bucket.bucket
-  key    = "token/${each.value}"
-  source = "${path.module}/../token/${each.value}"
+  bucket   = aws_s3_bucket.s3_bucket.bucket
+  key      = "token/${each.value}"
+  source   = "${path.module}/../token/${each.value}"
   # etag makes the file update when it changes; see https://stackoverflow.com/questions/56107258/terraform-upload-file-to-s3-on-every-apply
-  etag   = filemd5("${path.module}/../token/${each.value}")
+  etag = filemd5("${path.module}/../token/${each.value}")
 }
 
 resource "aws_s3_object" "dist_32x32" {
   for_each = fileset("${path.module}/../token/32x32/", "*")
-  bucket = aws_s3_bucket.s3_bucket.bucket
-  key    = "token/32x32/${each.value}"
-  source = "${path.module}/../token/32x32/${each.value}"
+  bucket   = aws_s3_bucket.s3_bucket.bucket
+  key      = "token/32x32/${each.value}"
+  source   = "${path.module}/../token/32x32/${each.value}"
   # etag makes the file update when it changes; see https://stackoverflow.com/questions/56107258/terraform-upload-file-to-s3-on-every-apply
-  etag   = filemd5("${path.module}/../token/32x32/${each.value}")
+  etag = filemd5("${path.module}/../token/32x32/${each.value}")
 }
 
 resource "aws_s3_object" "dist_96x96" {
   for_each = fileset("${path.module}/../token/96x96/", "*")
-  bucket = aws_s3_bucket.s3_bucket.bucket
-  key    = "token/96x96/${each.value}"
-  source = "${path.module}/../token/96x96/${each.value}"
+  bucket   = aws_s3_bucket.s3_bucket.bucket
+  key      = "token/96x96/${each.value}"
+  source   = "${path.module}/../token/96x96/${each.value}"
   # etag makes the file update when it changes; see https://stackoverflow.com/questions/56107258/terraform-upload-file-to-s3-on-every-apply
-  etag   = filemd5("${path.module}/../token/96x96/${each.value}")
+  etag = filemd5("${path.module}/../token/96x96/${each.value}")
+}
+
+
+resource "aws_s3_object" "social" {
+  for_each = fileset("${path.module}/../social/", "*")
+  bucket   = aws_s3_bucket.s3_bucket.bucket
+  key      = "social/${each.value}"
+  source   = "${path.module}/../social/${each.value}"
+  # etag makes the file update when it changes; see https://stackoverflow.com/questions/56107258/terraform-upload-file-to-s3-on-every-apply
+  etag = filemd5("${path.module}/../social/${each.value}")
+}
+
+resource "aws_s3_object" "network" {
+  for_each = fileset("${path.module}/../network/", "*")
+  bucket   = aws_s3_bucket.s3_bucket.bucket
+  key      = "network/${each.value}"
+  source   = "${path.module}/../network/${each.value}"
+  # etag makes the file update when it changes; see https://stackoverflow.com/questions/56107258/terraform-upload-file-to-s3-on-every-apply
+  etag = filemd5("${path.module}/../network/${each.value}")
 }
